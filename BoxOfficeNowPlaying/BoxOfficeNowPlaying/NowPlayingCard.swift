f0d0fdaf7a492a010eb7950dd73811f08a542846
nodediggity <nodediggity@icloud.com>
2020-08-21 17:17:09 +0100

    feat: model `NowPlayingCard`

---
+++ BoxOfficeNowPlaying/BoxOfficeNowPlaying/NowPlayingCard.swift
@@ -0,0 +1,15 @@
+//
+//  NowPlayingCard.swift
+//  BoxOfficeNowPlaying
+//
+//  Created by Gordon Smith on 21/08/2020.
+//  Copyright © 2020 Gordon Smith. All rights reserved.
+//
+
+import Foundation
+
+struct NowPlayingCard {
+  let id: Int
+  let title: String
+  let imagePath: String
+}
