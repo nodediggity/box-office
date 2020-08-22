42cd7c4d053accb5b628b6775119d63cef269d5f
nodediggity <nodediggity@icloud.com>
2020-08-22 09:55:14 +0100

    chore: create now playing iOS module

---
+++ BoxOfficeNowPlaying/BoxOfficeNowPlayingiOS/BoxOfficeNowPlayingiOS.h
@@ -0,0 +1,19 @@
+//
+//  BoxOfficeNowPlayingiOS.h
+//  BoxOfficeNowPlayingiOS
+//
+//  Created by Gordon Smith on 22/08/2020.
+//  Copyright © 2020 Gordon Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoxOfficeNowPlayingiOS.
+FOUNDATION_EXPORT double BoxOfficeNowPlayingiOSVersionNumber;
+
+//! Project version string for BoxOfficeNowPlayingiOS.
+FOUNDATION_EXPORT const unsigned char BoxOfficeNowPlayingiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxOfficeNowPlayingiOS/PublicHeader.h>
+
+
---
+++ BoxOfficeNowPlaying/BoxOfficeNowPlayingiOSTests/BoxOfficeNowPlayingiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoxOfficeNowPlayingiOSTests.swift
+//  BoxOfficeNowPlayingiOSTests
+//
+//  Created by Gordon Smith on 22/08/2020.
+//  Copyright © 2020 Gordon Smith. All rights reserved.
+//
+
+import XCTest
+@testable import BoxOfficeNowPlayingiOS
+
+class BoxOfficeNowPlayingiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
