af2e4a4fd625ca4559a092a6f93c6ac91a68331d
nodediggity <nodediggity@icloud.com>
2020-08-23 11:03:32 +0100

    chore: create initial iOS app project

---
+++ BoxOfficeApp/BoxOfficeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoxOfficeApp
+//
+//  Created by Gordon Smith on 23/08/2020.
+//  Copyright © 2020 Gordon Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
