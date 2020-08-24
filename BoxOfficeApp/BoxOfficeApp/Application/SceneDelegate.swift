f0e63ddbbfb3f2c18cfbcbcf5984ab0cd7ce0760
nodediggity <nodediggity@icloud.com>
2020-08-24 19:16:21 +0100

    feat: on select now playing card push detail view

--- BoxOfficeApp/BoxOfficeApp/Application/SceneDelegate.swift
+++ BoxOfficeApp/BoxOfficeApp/Application/SceneDelegate.swift
@@ -9,23 +9,32 @@
 import UIKit
 import BoxOfficeNetworking
 import BoxOfficeMedia
+
 import BoxOfficeNowPlaying
 import BoxOfficeNowPlayingiOS
+
+import BoxOfficeMovieDetails
+import BoxOfficeMovieDetailsiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
   private lazy var config: AuthConfig = getConfig(fromPlist: "AuthConfig")
+  private lazy var navController = UINavigationController()
+
+  private lazy var baseURL = URL(string: "https://api.themoviedb.org")!
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+    UIFont.loadCustomFonts
 
     guard let windowScene = (scene as? UIWindowScene) else { return }
 
     let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = UINavigationController(rootViewController: makeNowPlayingScene())
 
-    UIFont.loadCustomFonts
+    navController.setViewControllers([makeNowPlayingScene()], animated: true)
+    window.rootViewController = navController
 
     self.window = window
     self.window?.makeKeyAndVisible()
@@ -38,11 +47,24 @@ private extension SceneDelegate {
     let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
     let authzClient = AuthenticatedHTTPClientDecorator(decoratee: client, config: config)
 
-    let loader = RemoteNowPlayingLoader(baseURL: URL(string: "https://api.themoviedb.org")!, client: authzClient)
+    let loader = RemoteNowPlayingLoader(baseURL: baseURL, client: authzClient)
     let imageLoader = RemoteImageDataLoader(client: client)
 
-    let viewController = NowPlayingUIComposer.compose(loader: loader, imageLoader: imageLoader, onSelectCallback: { _ in })
+    let viewController = NowPlayingUIComposer.compose(loader: loader, imageLoader: imageLoader, onSelectCallback: { [weak self] movieID in
+      guard let self = self else { return }
+      let detailsViewController = self.makeMovieDetailScene(for: movieID)
+      self.navController.pushViewController(detailsViewController, animated: true)
+    })
 
+    return viewController
+  }
+
+  func makeMovieDetailScene(for id: Int) -> MovieDetailsViewController {
+    let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
+    let authzClient = AuthenticatedHTTPClientDecorator(decoratee: client, config: config)
+
+    let loader = RemoteMovieLoader(baseURL: baseURL, client: authzClient)
+    let viewController = MovieDetailsUIComposer.compose(id: id, loader: loader, onPurchaseCallback: { })
     return viewController
   }
 }
