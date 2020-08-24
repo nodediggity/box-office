58d37b363eeac145ea5393882397b3e7eac613fd
nodediggity <nodediggity@icloud.com>
2020-08-24 17:16:22 +0100

    test: request movie on view did load

--- BoxOfficeMovieDetails/BoxOfficeMovieDetailsiOSTests/Controllers/MovieDetailsViewControllerTests.swift
+++ BoxOfficeMovieDetails/BoxOfficeMovieDetailsiOSTests/Controllers/MovieDetailsViewControllerTests.swift
@@ -7,24 +7,44 @@
 //
 
 import XCTest
+import BoxOfficeMovieDetails
 
-final class MovieDetailsViewController {
+final class MovieDetailsViewController: UIViewController {
 
+  private var id: Int?
+  private var loader: MovieLoader?
+
+  convenience init(id: Int, loader: MovieLoader) {
+    self.init(nibName: nil, bundle: nil)
+    self.id = id
+    self.loader = loader
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    loader?.load(id: id!, completion: { _ in })
+  }
 }
 
 class MovieDetailsViewControllerTests: XCTestCase {
 
   func test_load_actions_request_details_from_loader() {
-    let (_, loader) = makeSUT()
+    let movieID = 100
+    let (sut, loader) = makeSUT(id: movieID)
     XCTAssertTrue(loader.messages.isEmpty)
+
+    sut.loadViewIfNeeded()
+    XCTAssertEqual(loader.messages, [.load(movieID)])
+
   }
 
 }
 
 private extension MovieDetailsViewControllerTests {
-  func makeSUT(file: StaticString = #file, line: UInt = #line) -> (MovieDetailsViewController, LoaderSpy) {
+  func makeSUT(id: Int = 0, file: StaticString = #file, line: UInt = #line) -> (MovieDetailsViewController, LoaderSpy) {
     let loader = LoaderSpy()
-    let sut = MovieDetailsViewController()
+    let sut = MovieDetailsViewController(id: id, loader: loader)
 
     checkForMemoryLeaks(loader, file: file, line: line)
     checkForMemoryLeaks(sut, file: file, line: line)
@@ -32,7 +52,18 @@ private extension MovieDetailsViewControllerTests {
     return (sut, loader)
   }
 
-  class LoaderSpy {
-    private(set) var messages: [Any] = []
+  class LoaderSpy: MovieLoader {
+
+    enum Message: Equatable {
+      case load(Int)
+    }
+
+    private(set) var messages: [Message] = []
+
+    typealias Result = MovieLoader.Result
+
+    func load(id: Int, completion: @escaping (Result) -> Void) {
+      messages.append(.load(id))
+    }
   }
 }
